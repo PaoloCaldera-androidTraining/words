a3deeb94a8470c3607c4f71ad0a1ea486790fcdf
Paolo Caldera <paolo.caldera@outlook.com>
2023-01-05 13:07:28 +0100

    Use preference value in LetterListFragment.kt

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -22,9 +22,17 @@ private val Context.datastore: DataStore<Preferences> by preferencesDataStore(
 
 class SettingsDataStore(context: Context) {
 
-    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+    companion object {
+        // Create a variable of type Preferences.Key<Boolean>, to store the key for a boolean value
+        private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+    }
 
+    /*  Preferences datastore exposes data in a Flow<Preferences> object, which emits the new
+        value everytime a preference has changed; to just expose the Boolean value, use the map
+        function which maps to preferenceFlow just the boolean value
+     */
     val preferenceFlow: Flow<Boolean> = context.datastore.data
+        // An IOException can occur when accessing the DataStore object: catch the error
         .catch {
             if (it is IOException) {
                 it.printStackTrace()
@@ -34,15 +42,18 @@ class SettingsDataStore(context: Context) {
             }
         }
         .map {
+            // On the first run, the boolean value is null, therefore map the default "true" value
             it[IS_LINEAR_LAYOUT_MANAGER] ?: true
         }
 
+    // Make the method a suspend function since edit() is executed in the Dispatcher.IO coroutine
     suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+
+        // edit(): function that transactionally updates the data in the DataStore object
         context.datastore.edit {
             it[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
         }
     }
-
 
 }
 
